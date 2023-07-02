d50482ae9ed203fcddfadefafd5b0a487f918d1a
Rohit Paul <113459757+RohitPaul12345@users.noreply.github.com>
2023-07-02 13:54:46 +0530

    Update 05 - Conditionals.js

--- LearnJavascript/Topics/05 - Conditionals.js
+++ LearnJavascript/Topics/05 - Conditionals.js
@@ -5,7 +5,7 @@ describe('Conditionals', function () {
   /*Conditionals*/
   /*Expressions that allow to execute code paths based on specific conditions*/
   it('If then Else', function () {
-    var shouldBeTrue;
+    let shouldBeTrue;
     //If the conditional evaluates to true, the code block is executed
     if (true) {
       shouldBeTrue = true;
@@ -16,7 +16,7 @@ describe('Conditionals', function () {
     expect(shouldBeTrue).toBe(true);
   });
   it('Else-If when two conditionals are not enough', function () {
-    var shouldBeTrue;
+    let shouldBeTrue;
     //If the conditional does not evaluates to true, the code block is not executed
     if (false) {
       shouldBeTrue = false;
@@ -28,7 +28,7 @@ describe('Conditionals', function () {
   });
   /*Complex Conditionals*/
   it('Binary AND', function () {
-    var value;
+    let value;
     //Binary AND returns true if All values are true
     value = true && false;
     expect(value).toBe(false);
@@ -41,7 +41,7 @@ describe('Conditionals', function () {
 
   });
   it('Binary OR', function () {
-    var value;
+    let value;
     //Binary OR returns true if Either value is true
     value = true || false;
     expect(value).toBe(true);
