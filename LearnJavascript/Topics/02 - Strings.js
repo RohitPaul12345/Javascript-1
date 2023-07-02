85c5c620b114f7fe643a109852d7368580eb69cd
Rohit Paul <113459757+RohitPaul12345@users.noreply.github.com>
2023-07-02 13:52:26 +0530

    Update 02 - Strings.js

--- LearnJavascript/Topics/02 - Strings.js
+++ LearnJavascript/Topics/02 - Strings.js
@@ -33,22 +33,22 @@ describe('Strings', function () {
 
   it('You can convert a number, Boolean or an object to string using the String() constructor', function () {
 
-    var boolean = false;
+    let boolean = false;
     expect(String(boolean)).toBe("false");
 
-    var number = 6;
+    let number = 6;
     expect(String(number)).toBe("6");
 
-    var object = {};
+    let object = {};
     expect(String(object)).toBe("[object Object]");
 
   });
 
   it('You can split a String into multiple sub-strings (inside an Array) using the split() method', function () {
 
-    var numbers = "0,1,2";
+    let numbers = "0,1,2";
     //split() method takes as an argument the separator
-    var numbersArray = numbers.split(',');
+    let numbersArray = numbers.split(',');
 
     expect(numbersArray[0]).toBe("0");
     expect(numbersArray[1]).toBe("1");
@@ -58,12 +58,12 @@ describe('Strings', function () {
   it('You can locate a Substring within a String using the indexOf() method', function () {
    //The indexOf() method starts searching for the substring (the first argument passed in) from the beginning of the string
    // The method returns the position of the start of the first occurrence of the substring.
-    var fullName = "Martin Chavez Aguilar";
+    let fullName = "Martin Chavez Aguilar";
     expect(fullName.indexOf("Chavez")).toBe(7);
   });
 
   it('You can replace a Substring using the replace() method', function () {
-    var fullName = "Marvin Chavez Aguilar";
+    let fullName = "Marvin Chavez Aguilar";
     //The first argument is the substring you want to replace, the second argument is the new substring.
     fullName = fullName.replace("Marvin","Martin");
     // This will only replace the first instance of the matched substring.
@@ -73,13 +73,13 @@ describe('Strings', function () {
   it('You can delete a Substring from a string using the slice() method', function () {
     //The first argument is the character at which to begin the substring (using zero-based numbering)
     //the second argument (which is optional) is the character in the string after you want the substring to end
-    var fullName = "Martin Chavez Aguilar";
+    let fullName = "Martin Chavez Aguilar";
     fullName = fullName.slice(7);
     expect(fullName).toBe("Chavez Aguilar");
   });
 
   it('You can convert a string to Uppercase or Lowercase using the toUpperCase() and toLowerCase() methods', function () {
-    var word = "ABC";
+    let word = "ABC";
     expect(word.toLowerCase()).toBe("abc");
   });
 
@@ -87,11 +87,11 @@ describe('Strings', function () {
 
   it('Use standard concatenation for small string concatenations', function () {
     //Standard concatenation operator has been optimized in most modern browser versions
-    var stringOne = "One";
-    var stringTwo = "Two";
-    var stringThree = "Three";
+    let stringOne = "One";
+    let stringTwo = "Two";
+    let stringThree = "Three";
 
-    var finalString = "";
+    let finalString = "";
     finalString += stringOne;
     finalString += stringTwo;
     finalString += stringThree;
@@ -101,18 +101,18 @@ describe('Strings', function () {
 
   it('You can also use concat() method for string concatenations', function () {
     //Standard concatenation operator has been optimized in most modern browser versions
-    var stringOne = "One";
-    var stringTwo = "Two";
-    var stringThree = "Three";
+    let stringOne = "One";
+    let stringTwo = "Two";
+    let stringThree = "Three";
 
-    var finalString = stringOne.concat(stringTwo,stringThree);
+    let finalString = stringOne.concat(stringTwo,stringThree);
 
     expect(finalString).toBe("OneTwoThree");
   });
 
   it('When strings are present in an array, use the join() method to concatenate', function () {
     //The join() method is inherited from the Array Prototype
-    var stringArrays = ["StringOne", "StringTwo", "StringThree"];
+    let stringArrays = ["StringOne", "StringTwo", "StringThree"];
     //Apart for performing better than string concatenation, the code becomes more readable
     expect(stringArrays.join("")).toBe("StringOneStringTwoStringThree");
   });
